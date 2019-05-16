455432f24c8245a90bd645400ed56c40fe227dfe
Diego Cuevas <diegocuevas579@gmail.com>
2019-05-16 17:01:05 -0500

    Complete the function

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,20 +1,33 @@
 const list_task = [
   {
     message: "Estudiar asdasd",
-    due_date: Date.parseDate("2017-06-12"),
-    creation_date: new Date.now(),
+    due_date: Date.parse("2017-06-12"),
+    creation_date: Date.now(),
     priority: true
   },
   {
     message: "Hacer Miniassignment",
-    due_date: Date.parseDate("2019-06-12"),
-    creation_date: new Date.now(),
+    due_date: Date.parse("2019-06-12"),
+    creation_date: Date.now(),
     priority: false
   }
 ];
 
 function save_taks() {
-  var message = "Lavar la ropa";
-  var due_date = new Date().now;
-  var priority = false;
+  var msg = "Lavar la ropa";
+  var due = Date.parse("2019-07-12");
+  var current_date = Date.now();
+  var prior = false;
+  var new_element = {
+    message: msg,
+    due_date: due,
+    creation_date: current_date,
+    priority: prior
+  }
+  list_task.push(new_element);
+  sessionStorage.setItem('list',list_task);
 }
+
+console.log(list_task);
+save_taks();
+console.log(list_task);
