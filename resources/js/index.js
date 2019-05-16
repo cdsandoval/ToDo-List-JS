5ff67b796ef861965e19d555ccefa20b66fb2e70
Brayan Ciudad <linzeur@gmail.com>
2019-05-16 16:38:32 -0500

    Add schema in js code

---
+++ resources/js/index.js
@@ -0,0 +1,20 @@
+const list_task = [
+  {
+    message: "Estudiar asdasd",
+    due_date: Date.parseDate("2017-06-12"),
+    creation_date: new Date.now(),
+    priority: true
+  },
+  {
+    message: "Hacer Miniassignment",
+    due_date: Date.parseDate("2019-06-12"),
+    creation_date: new Date.now(),
+    priority: false
+  }
+];
+
+function save_taks() {
+  var message = "Lavar la ropa";
+  var due_date = new Date().now;
+  var priority = false;
+}
