bc12f9ee2ca8a5f41946286e4f996c5f9520eff9
Valeria Vassallo <valevassallo1@gmail.com>
2019-05-17 16:55:35 -0500

    Add validations v01

--- resources/js/validations.js
+++ resources/js/validations.js
@@ -1,27 +1,15 @@
 function validate() {
-  //name
-  if (document.todoform.field1.value == "") {
-    alert("Please provide your name!");
-    document.todoform.field1.focus();
-    return false;
-  }
-  if (document.todoform.field2.value == "") {
-    alert("Please provide your name!");
-    document.todoform.field2.focus();
-    return false;
-  }
   // message
-  if (document.todoform.field5.value == "") {
+  if (document.todoForm.field5.value == "") {
     alert("Please write a message!");
-    document.todoform.field5.focus();
+    document.todoForm.field5.focus();
     return false;
   }
   // due date
-  if (document.todoform.field6.value == "") {
+  if (document.todoForm.dueDate.value == "") {
     alert("Please determine a due date!");
-    document.todoform.field6.focus();
+    document.todoForm.dueDate.focus();
     return false;
   }
-
   return true;
 }
